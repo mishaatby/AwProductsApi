df15ab1d24914ebb3fd134b29c0c7daebbce790c
Mikhail Rafalovich <mikhail_rafalovich@epam.com>
2020-11-11 11:16:15 +0300

    initial commit

---
+++ ProductsApi/ProductsApi/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace ProductsApi.Middleware
+{
+    public class ErrorLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.TraceError("Unhandled exception: {0}", e.Message);
+                Log.Logger.Error("Problem occurred: {0}", e.Message);
+                throw;
+            }
+        }
+    }
+}
